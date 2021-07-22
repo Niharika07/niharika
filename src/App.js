ef97aeb40ae21329a75695f64efa1f05bc0fa190
Niharika07 <niharika.sharma07@outlook.com>
2021-07-22 21:14:45 +0530

    FIRST COMMIT

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import "./App.css"
 class LambdaDemo extends Component {
   constructor(props) {
     super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
+    
   }
 
   render() {
@@ -22,10 +13,7 @@ class LambdaDemo extends Component {
 
     return (
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
+        HELLLOOOO
       </p>
     )
   }
